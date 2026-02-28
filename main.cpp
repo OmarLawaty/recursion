b963df5fd4e98285ee665d0b97f32d9fd54e7a00
OmarLawaty <omarlawatey@gmail.com>
2026-02-28 23:33:41 +0200

    feat: Solve Problem 10

--- main.cpp
+++ main.cpp
@@ -1,35 +1,19 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-bool isVowel(char ch) {
-  string vowels = "aeiouAEIOU";
-  for (int i = 0; i < vowels.size(); ++i) {
-    if (ch == vowels[i])
-      return true;
-  }
+long long factorial(long long n) {
+  if (n <= 1)
+    return 1;
 
-  return false;
-}
-
-void countVowels(const string &s, int idx, int count) {
-  if (idx == s.size()) {
-    cout << count << '\n';
-    return;
-  }
-
-  if (isVowel(s[idx]))
-    count++;
-
-  countVowels(s, idx + 1, count);
+  return n * factorial(n - 1);
 }
 
 int main() {
-  string s;
-  if (!getline(cin, s))
+  long long n;
+  if (!(cin >> n))
     return 0;
 
-  countVowels(s, 0, 0);
+  cout << factorial(n) << '\n';
   return 0;
 }
