5342d282e0e08de1f9b21cdf527d9e068af624cb
OmarLawaty <omarlawatey@gmail.com>
2026-02-28 23:43:55 +0200

    feat: Solve Problem 11

--- main.cpp
+++ main.cpp
@@ -2,18 +2,30 @@
 
 using namespace std;
 
-long long factorial(long long n) {
-  if (n <= 1)
-    return 1;
+long long maxRecursive(int arr[], int idx, int size) {
+  if (idx == size - 1)
+    return arr[idx];
 
-  return n * factorial(n - 1);
+  long long tailMax = maxRecursive(arr, idx + 1, size);
+
+  if (arr[idx] > tailMax)
+    return arr[idx];
+
+  return tailMax;
 }
 
 int main() {
-  long long n;
+  int n;
   if (!(cin >> n))
     return 0;
 
-  cout << factorial(n) << '\n';
+  int arr[n];
+  for (int i = 0; i < n; ++i)
+    cin >> arr[i];
+
+  if (n == 0)
+    return 0;
+
+  cout << maxRecursive(arr, 0, n) << '\n';
   return 0;
 }
